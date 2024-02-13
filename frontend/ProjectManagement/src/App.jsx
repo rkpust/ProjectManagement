8891d84d48db121eb3788231f456f41e68865d47
rkpust <rezaul.cse.pust17@gmail.com>
2024-02-13 13:04:51 +0600

    installed and configured

---
+++ frontend/ProjectManagement/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Create from './components/Create';
+import NavBar from './components/NavBar';
+
+function App() {
+  const drawerWidth = 220;
+  return (
+    <div className='App'>
+      <NavBar drawerWidth={drawerWidth}
+      content = {
+        <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/create' element={<Create />} />
+      </Routes>
+      }
+      />
+    </div>
+  )
+}
+
+export default App
